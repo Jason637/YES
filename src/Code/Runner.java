b3d5c15534771d9bf3ccbd134667a6b22ef08dce
Jason637 <Jason.cheng64@yahoo.com>
2018-01-11 08:56:32 -0600

    This added the fraction calculations and prints them out as well as the multiplication method.

---
+++ src/Code/Runner.java
@@ -0,0 +1,16 @@
+package Code;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        Fraction myFraction = new Fraction(2,3);
+        Fraction otherFraction = new Fraction(5);
+        Fraction a = new Fraction(myFraction);
+        Fraction multiplied = myFraction.multiply(otherFraction);
+        System.out.println(myFraction);
+        System.out.println(a.equals(myFraction));
+        System.out.println(multiplied);
+
+    }
+}
